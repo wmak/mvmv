8a1555eb3bf2d6373815ee61e34fca8d35d34812
wmak <william.mak@hotmail.ca>
2014-11-14 16:15:07 -0500

    adding sockets to interact with the daemon

--- mvmv/mvmvd.py
+++ mvmv/mvmvd.py
@@ -1,26 +1,40 @@
 from daemon import Daemon
-from watchdog.observers import Observer
-import time
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import socket
+import time
 
 class MyHandler(FileSystemEventHandler):
-    def __init__(self):
-        self.modified = False
-
     def on_modified(self, event):
-        self.modified = True
+        print("modified") # mv code here
 
 class mvmv(Daemon):
     def run(self):
         self.observer = Observer()
-        self.monitors = [self.new_monitor(".", True)]
+        self.monitors = []
+        self.dirs = []
         self.observer.start()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("", 4242))
+        s.listen(5)
+
+        #main execution loop
         while True:
-            while not all([mon.modified for mon in self.monitors]):
-                time.sleep(1)
-            for mon in self.monitors:
-                mon.modified = False
-                print("modified") # Change to the move function
+            conn, addr = s.accept()
+            data = conn.recv(1024)
+            if data:
+                opt = data.split(" ")
+                message = ""
+                if opt[0] == "watch":
+                    if opt[-1] not in self.dirs:
+                        self.monitors.append(self.new_monitor(opt[-1],
+                            "-r" in opt))
+                        self.dirs.append(opt[-1])
+                        message = "added monitor %s" % opt[-1]
+                    else:
+                        message = "directory already being monitored"
+                conn.sendall(message)
+                conn.close()
 
     def new_monitor(self, path, recursive):
         event_handler = MyHandler()
