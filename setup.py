5ac84cea924935a05f5703383231f33ce455b98a
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2014-11-20 21:51:11 -0500

    Add setup.py and make README a rst file.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme  =  f.read()
+
+setup(
+    name = "mvmv",
+    version = "0.1",
+
+    author = "William Mak",
+    author_email = "WilliamSYMak@gmail.com",
+    url = "https://github.com/wmak/mvmv",
+
+
+    description = "Movie Mover - move your movies where they belong.",
+    long_description = readme,
+    license = 'MIT',
+
+    packages = find_packages(exclude=['*.tests']),
+    test_suite = "mvmv.tests",
+    install_requires = [
+        "fuzzywuzzy >= 0.4.0",
+        "python-Levenshtein >= 0.11.2",
+        "watchdog >= 0.8.2",
+    ],
+)
